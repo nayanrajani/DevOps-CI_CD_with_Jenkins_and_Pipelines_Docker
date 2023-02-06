5788445ebd828dbd6ffc3029d30c197efb591712
Nayan Rajani <nayanrajani86@gmail.com>
2023-02-06 18:23:32 +0530

    commit

---
+++ jenkins.sh
@@ -0,0 +1,86 @@
+sudo apt update
+sudo update
+sudo apt update
+sudo apt-get update
+sudo apt update
+sudo apt upgrade
+sudo apt update
+sudo apt upgrade
+clear
+sudo apt install bpython
+sudo apt update
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+sudo apt update
+sudo apt install bpython
+bpython
+clear
+sudo yum install wget
+sudo apt install wget
+yum install java-1.8.0-openjdk
+sudo apt install yum
+java -version
+sudo apt install openjdk-11-jdk
+java -version
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+systemctl status Jenkins
+systemctl start jenkins
+systemctl status Jenkins
+sudo systemctl start
+systemctl status Jenkins
+sudo systemctl start
+sudo systemctl start jenkins
+systemctl start jenkins
+systemctl restart jenkins
+systemctl enable service.jenkins
+systemctl enable jenkins
+systemctl start jenkins
+systemctl restart jenkins
+systemctl status Jenkins
+service status jenkins
+sudo service status jenkins
+sudo service start jenkins
+apt-get install runit-systemd
+sudo apt-get install runit-systemd
+sudo service status jenkins
+systemctl status Jenkins
+ps -p 1 -o comm=
+init
+systemctl init
+systemctl status Jenkins
+clear
+sudo -b unshare --pid --fork --mount-proc /lib/systemd/systemd --system-unit=basic.target
+sudo -E nsenter --all -t $(pgrep -xo systemd) runuser -P -l $USER -c "exec $SHELL"
+wsl --shutdown
+sudo apt install wsl
+wsl --shutdown
+systemctl status Jenkins
+sudo apt-get update jenkins
+sudo apt update jenkins
+apt update jenkins
+apt-install update jenkins
+apt-get update jenkins
+apt upgrade jenkins
+sudo systemctl docker start
+sudo systemctl jenkins start
+service ssh start
+sudo apt install jenkins
+sudo systemctl status jenkins
+sudo systemctl start jenkins
+clear
+sudo ufw allow 8080
+sudo ufw status
+sudo ufw enable
+sudo ufw status
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo ufw status
+sudo systemctl start jenkins
+systemctl status jenkins.service
+sudo systemctl stop jenkins
+sudo systemctl stop jenkins.service
+systemctl status jenkins.service
